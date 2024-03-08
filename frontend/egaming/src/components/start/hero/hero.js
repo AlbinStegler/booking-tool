4c9aeb8fb6d5e3c6a50dd7c5f465f9bc5e74d5a6
Albin Stegler <albin.stegler@gmail.com>
2024-03-08 10:36:40 +0100

    path changed

--- frontend/egaming/src/components/start/hero/hero.js
+++ frontend/egaming/src/components/start/hero/hero.js
@@ -6,7 +6,7 @@ const Hero = () => {
         <div className="hero">
             <video className="background-video" autoPlay loop muted>
 
-                <source src="images/first_export_long.webm" type="video/webm" alt="backgroundvideo" />
+                <source src={process.env.PUBLIC_URL + "images/first_export_long.webm"} type="video/webm" alt="backgroundvideo" />
             </video>
             <div className='hero-content'>
                 <img src="images/Banner.png" alt="banner" />
